6ed25290dbf38b74d71e8f53e4cec6c51b7ceab3
Valerii <vd.alternative@gmail.com>
2024-02-29 11:51:09 +0200

    second task

--- src/homework/1.tsx
+++ src/homework/1.tsx
@@ -4,6 +4,12 @@ type Props = {
   children: ReactNode;
   onContentEndVisible: () => void;
 }
+
+interface Options {
+  rootMargin: string;
+  threshold: number;
+  root: null;
+}
 // Опишіть Props
 export function Observer({ children, onContentEndVisible }: Props) {
   // Вкажіть правильний тип для useRef зверніть увагу, в який DOM елемент ми його передаємо
@@ -11,7 +17,7 @@ export function Observer({ children, onContentEndVisible }: Props) {
 
   useEffect(() => {
     // Вкажіть правильний тип для options, підказка, клас також можна вказувати як тип
-    const options = {
+    const options: Options = {
       rootMargin: '0px',
       threshold: 1.0,
       root: null,
